b762b6797a2b3e17a8739b67743c00f449498caa
Hamza EL Yousfi <Hamza.El_Yousfi@bordeaux-inp.fr>
2022-07-18 22:17:20 +0200

    ok

--- KeypointsDetection/KeypointsDetection/keypDetec/Source.cpp
+++ KeypointsDetection/KeypointsDetection/keypDetec/Source.cpp
@@ -3,12 +3,34 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
+
 using namespace cv;
+using namespace std;
+
 
 int main()
 {
-    cv::Mat img = cv::imread("image.JPG");
-    cv::imshow("ok", img);
-    cv::waitKey(0);
+    /// 
+    /// Read an image
+    /// 
+    string path = "image.JPG";
+    Mat img = imread(path);
+    imshow("ok", img);
+    waitKey(0);
+    
+    /// 
+    /// Read a video
+    /// 
+    string path_video = "video.mp4";
+    VideoCapture cap(path_video);
+    Mat frame;
+
+    while (true) {
+        cap.read(frame);
+        imshow("Image", frame);
+        waitKey(1);
+    }
+
+
     return 0;
-}
+}   
